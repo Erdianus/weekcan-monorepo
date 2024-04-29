4ecd2f042245278a688b0931f5cccb4ea79c601c
oktoala <okta.yoga.syah@gmail.com>
2024-04-29 10:52:49 +0800

    make timeline page

--- apps/weekcan/app/(auth)/project/[project_id]/single.tsx
+++ apps/weekcan/app/(auth)/project/[project_id]/single.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client';
 
-import k from "@repo/api/kit";
-import { Skeleton } from "@ui/components/ui/skeleton";
-import { H2 } from "@ui/components/ui/typograhpy";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useMemo } from "react";
+import { useMemo } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Skeleton } from '@ui/components/ui/skeleton';
+import { H2 } from '@ui/components/ui/typograhpy';
+
+import k from '@repo/api/kit';
 
 const SingleProject = ({ id }: { id: string | number }) => {
   const pathname = usePathname();
@@ -13,28 +14,28 @@ const SingleProject = ({ id }: { id: string | number }) => {
 
   const links = useMemo(() => {
     return [
-      { label: "Detail", href: `/project/${id}` },
-      { label: "Tugas", href: `/project/${id}/task` },
-      { label: "Kustom", href: `/project/${id}/custom` },
-      { label: "Files", href: `/project/${id}/files` },
-      { label: "Anggota", href: `/project/${id}/member` },
-      { label: "Jadwal", href: `/project/${id}/timeline` },
+      { label: 'Detail', href: `/project/${id}` },
+      { label: 'Tugas', href: `/project/${id}/task` },
+      { label: 'Kustom', href: `/project/${id}/custom` },
+      { label: 'Files', href: `/project/${id}/files` },
+      { label: 'Anggota', href: `/project/${id}/member` },
+      { label: 'Jadwal', href: `/project/${id}/sprint` },
     ];
   }, [id]);
 
   return (
     <>
-      <H2 className="border-b-0 mb-4">
-        {project?.data.project_name ?? <Skeleton className="w-24 h-8" />}
+      <H2 className="mb-4 border-b-0">
+        {project?.data.project_name ?? <Skeleton className="h-8 w-24" />}
       </H2>
-      <div className="flex max-w-min min-w-0 w-auto mb-4 h-10 items-center rounded-md bg-muted p-1 text-muted-foreground overflow-x-auto">
+      <div className="mb-4 flex h-10 w-auto min-w-0 max-w-min items-center overflow-x-auto rounded-md bg-muted p-1 text-muted-foreground">
         {links.map((link) => (
           <Link
             replace
             key={link.label}
             href={link.href}
-            data-state={pathname === link.href ? "active" : "inactive"}
-            className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-main-500 data-[state=active]:text-white dark:data-[state=active]:bg-main-900 data-[state=active]:text-400 data-[state=active]:shadow-sm"
+            data-state={pathname === link.href ? 'active' : 'inactive'}
+            className="data-[state=active]:text-400 inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-main-500 data-[state=active]:text-white data-[state=active]:shadow-sm dark:data-[state=active]:bg-main-900"
           >
             {link.label}
           </Link>
---
+++ apps/weekcan/app/(auth)/project/[project_id]/sprint/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Jadwal Proyek',
+};
+
+export default async function SprintProjectPage() {
+  return <>Sprint</>;
+}
