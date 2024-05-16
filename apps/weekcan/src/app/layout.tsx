ed10183e2dc1b8a1ba83c9d13a7f3193be90f374
oktoala <okta.yoga.syah@gmail.com>
2024-05-16 16:18:18 +0800

    test

--- apps/weekcan/src/app/layout.tsx
+++ apps/weekcan/src/app/layout.tsx
@@ -18,7 +18,6 @@ import { env } from "~/env";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(env.BASE_URL),
   title: {
     default: "weekcan",
     template: "%s - weekcan",
