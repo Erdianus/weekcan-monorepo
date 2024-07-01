562376dd5d8f6919cc6adc5882b3136766c27001
oktoala <okta.yoga.syah@gmail.com>
2024-07-01 10:19:39 +0800

    fix lint

--- apps/weekcan/src/app/(auth)/error.tsx
+++ apps/weekcan/src/app/(auth)/error.tsx
@@ -3,11 +3,8 @@
 // Error components must be Client Components
 import { useEffect } from "react";
 
-import { Button } from "@hktekno/ui/components/ui/button";
-
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
