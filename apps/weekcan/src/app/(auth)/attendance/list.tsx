1cbcb3d3e6f8832a76ff5bcbeda097d1b308fc9f
oktoala <okta.yoga.syah@gmail.com>
2024-08-20 10:44:07 +0800

    add lat long to gmap link

--- apps/weekcan/src/app/(auth)/attendance/list.tsx
+++ apps/weekcan/src/app/(auth)/attendance/list.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import {
+  CellContext,
   createColumnHelper,
   getCoreRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import dayjs from "dayjs";
 import { atom, useAtom, useSetAtom } from "jotai";
+import { Check, Copy, MapPinned, MoreHorizontal } from "lucide-react";
 
 import type { inferData } from "@hktekno/api";
 import { k } from "@hktekno/api";
@@ -17,8 +19,17 @@ import { LightBox } from "@hktekno/ui/components/lightbox";
 import Paginate from "@hktekno/ui/components/paginate";
 import { PaginationMore } from "@hktekno/ui/components/pagination-params";
 import { Badge } from "@hktekno/ui/components/ui/badge";
+import { Button } from "@hktekno/ui/components/ui/button";
 import { DataTable } from "@hktekno/ui/components/ui/data-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@hktekno/ui/components/ui/dropdown-menu";
 import { Muted } from "@hktekno/ui/components/ui/typograhpy";
+import { getDMS } from "@hktekno/ui/lib/utils";
 
 import FilterAttendance from "./filter";
 
@@ -29,6 +40,64 @@ type Attendance = inferData<
 const colHelper = createColumnHelper<Attendance>();
 
 const openAtom = atom(false);
+
+const Action = ({ row }: CellContext<Attendance, unknown>) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const { latitude, longitude } = row.original;
+  const dms = getDMS(Number(latitude), Number(longitude)).join("+");
+  // This is the function we wrote earlier
+  async function copyTextToClipboard(text: string) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    }
+  }
+
+  // onClick handler function for the copy button
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(`${latitude},${longitude}`)
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild disabled={isCopied}>
+          <Button variant="ghost" className="h-8 w-8 p-0" disabled={isCopied}>
+            <span className="sr-only">Open menu</span>
+            {isCopied ? <Check /> : <MoreHorizontal className="h-4 w-4" />}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={handleCopyClick}>
+            <Copy className="mr-2 h-4 w-4" />
+            <span>Salin Lat/Long</span>
+          </DropdownMenuItem>
+
+          <a
+            target="_blank"
+            href={`https://www.google.com/maps/search/${latitude},${longitude}/@${latitude},${longitude}`}
+          >
+            <DropdownMenuItem>
+              <MapPinned className="mr-2 h-4 w-4" />
+              <span>Lihat di Gmap</span>
+            </DropdownMenuItem>
+          </a>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
 
 const columns = [
   colHelper.display({
@@ -68,6 +137,10 @@ const columns = [
     cell: ({ getValue }) => (
       <div className="whitespace-nowrap">{getValue()}</div>
     ),
+  }),
+  colHelper.display({
+    id: "actions",
+    cell: Action,
   }),
 ];
 
